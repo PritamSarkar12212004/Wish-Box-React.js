5c3f78b0e1dcd2e2b08830eeac7d1fe611f05d14
pritam sarkar <pritam7796419792@gmail.com>
2024-09-10 22:05:39 +0530

    on the way

--- src/components/pages/Landing/sections/Section3.jsx
+++ src/components/pages/Landing/sections/Section3.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 function Section3() {
   return (
-    <div className="w-full flex  justify-between items-center  px-10 py-10 text-white  bg-[#F42C37] h-96 rounded-3xl mt-20 mb-20 relative ">
-      <span>
+    <div className="w-full flex md:flex-row flex-col  md:justify-between md:items-center  justify-center items-center lg:px-10 lg:h-auto md:h-50  md:px-5   lg:py-10   md:text-white  md:bg-[#F42C37] h-96 rounded-3xl md:mt-20 md:mb-20 mb-96 relative text-black ">
+      <span className="md:m-0 mt-[80vh]">
         <h1>30% OFF</h1>
-        <h1 className="text-6xl font-bold leading-tight">
+        <h1 className="lg:text-6xl lg:font-bold lg:leading-tight md:text-[5vw] md:font-bold md:font-mono">
           Brighten <br /> Diwali <br /> with Paper!
         </h1>
       </span>
       <img
-        src="src/assets/poster/section3--.png"
+        src="src/assets/poster/section3.png"
         alt=""
-        className="absolute inset-0 m-auto bottom-24"
+        className="absolute inset-0 m-auto lg:bottom-24 md:bottom-60 lg:h-auto md:w-[50vw] lg:w-auto"
       />
-      <h1 className="text-6xl font-bold leading-tight">
-        Shop With <br /> Wish Box <br />
+      <h1 className="lg:text-6xl lg:font-bold lg:leading-tight md:text-[5vw] md:font-bold md:font-mono">
+        Shop With <br /> Wish Box <br /> 
         <span className="text-yellow-400">
           <i class="ri-shopping-bag-fill"></i>
         </span>
