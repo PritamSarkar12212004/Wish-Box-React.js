e959a965f64c0bb7881737413fe607577164283e
pritam sarkar <pritam7796419792@gmail.com>
2024-09-11 00:24:13 +0530

    section 3 done

--- src/components/pages/Landing/sections/Section3.jsx
+++ src/components/pages/Landing/sections/Section3.jsx
@@ -2,28 +2,22 @@ import React from "react";
 
 function Section3() {
   return (
-    <div className="w-full flex md:flex-row flex-col  md:justify-between md:items-center  justify-center items-center lg:px-10 lg:h-auto  md:px-10 md:py-20   lg:py-10   md:text-white  md:bg-[#F42C37]   rounded-3xl md:mt-20 md:mb-20 mb-96 relative text-black ">
+    <div className="w-full flex md:flex-row flex-col  md:justify-between md:items-center  justify-center items-center lg:px-10 lg:h-auto  md:px-10 md:py-20   lg:py-10   md:text-white  md:bg-[#F42C37]   rounded-3xl md:mt-20 md:mb-20 relative text-black ">
       <img
         src="src/assets/poster/section3.png"
         alt=""
         className=" md:absolute inset-0 m-auto lg:bottom-24 md:bottom-60 lg:h-auto md:w-[50vw] lg:w-auto"
       />
-      <div className="w-full flex md:flex-row flex-col  md:justify-between justify-around md:text-start text-center text-3xl">
-        <span className="md:m-0 w-full md:flex flex-col hidden ">
+      <div className="w-full flex  md:flex-row flex-col  md:justify-between justify-around md:text-start text-center text-3xl">
+        <span className="md:m-0 w-full md:flex flex-col  ">
           <h1>30% OFF</h1>
           <h1 className="lg:text-6xl lg:font-bold lg:leading-tight md:text-[5vw] md:font-bold md:font-mono">
             Brighten <br /> Diwali <br /> with Paper!
           </h1>
         </span>
-        <span className="md:m-0 w-full flex justify-center text-center items-center ">
-          <h1>30% OFF</h1> <br />
-          <h1 className="lg:text-6xl lg:font-bold lg:leading-tight md:text-[5vw] md:font-bold md:font-mono">
-             Brighten Diwali with Paper!
-          </h1>
-        </span>
 
         <h1 className="lg:text-6xl lg:font-bold lg:leading-tight md:text-[5vw] md:font-bold md:font-mono">
-          Shop With <br /> Wish Box <br />
+          Shop With Wish Box
           <span className="text-yellow-400">
             <i class="ri-shopping-bag-fill"></i>
           </span>
--- src/components/pages/Landing/sections/Section4.jsx
+++ src/components/pages/Landing/sections/Section4.jsx
@@ -3,52 +3,52 @@ import Avatars from "../../../layouts/Avatar/Avatars";
 
 function Section4() {
   return (
-    <div className="w-full h-screen flex justify-between gap-5">
-      <div className="h-screen w-[35%] flex flex-col justify-between ">
-        <div className="w-full h-[47%] bg-blue-500 rounded-3xl flex justify-center items-center">
+    <div className="w-full lg:h-full  flex md:flex-row flex-col justify-between gap-5">
+      <div className="h-full md:w-[35%] flex flex-col justify-between md:items-start items-center  gap-10 ">
+        <div className="md:w-full w-80 h-[47%] hidden bg-blue-500 rounded-3xl md:flex justify-center items-center">
           <img
             src="src/assets/poster/11.png"
             className="h-full hover:scale-125 duration-300 cursor-pointer "
             alt=""
           />
         </div>
-        <div className="w-full h-[47%] bg-[#cca3ff] rounded-3xl px-10 py-10 text-4xl font-bold ">
+        <div className="md:w-full w-96 h-[47%] bg-[#cca3ff] rounded-3xl px-10 py-10 lg:text-4xl md:text-3xl font-bold ">
           <h1 className="opacity-70">GET READY FOR SOMTHING NEW PRODUCT !</h1>
           <button className="h-16 w-16 border-2 rounded-full border-black mt-10  hover:rotate-180 duration-300 ">
             <i class="ri-arrow-right-down-line"></i>
           </button>
         </div>
       </div>
-      <div className="h-screen w-[40%]  flex flex-col justify-between">
+      <div className="h-full md:w-[40%] w-full  flex flex-col justify-between md:items-start items-center gap-20">
         <div className=" px-10 py-10 w-full h-[60%] bg-[#ffe0d3] rounded-3xl">
-          <span className=" flex w-full h-16  items-center text-4xl font-bold gap-4">
+          <span className=" flex w-full h-16  items-center  md:text-2xl lg:text-4xl font-bold gap-4">
             EXPLORE <Avatars /> <br />
           </span>
-          <span className=" flex w-full h-16 r text-4xl font-bold">
+          <span className=" flex w-full h-16 md:text-2xl lg:text-4xl font-bold">
             Elevate Every Occasion with Stylish Paper Decoration!
           </span>
-          <p className="mt-20 text-sm">
+          <p className="md:mt-20 lg:text-sm md:text-medium">
             Our paper products are thoughtfully crafted to bring a unique and
             stylish touch to every celebration. Whether it's a birthday,
-            wedding, or corporate event, our designs add elegance and charm to
+            wedding, or corporate event, our designs add elegance and charm to <br />
             
             <button className="px-4 mt-3 py-2 bg-orange-500 rounded-3xl text-white">
               Shop Now
             </button>
           </p>
         </div>
-        <div className="w-full h-[35%] bg-[#ffcf49] rounded-3xl px-5 py-5 flex items-center justify-around">
+        <div className="md:w-full w-96 h-[17vh] bg-[#ffcf49] rounded-3xl px-5 py-5 flex items-center justify-around">
           <div className="flex flex-col">
-            <span className="text-5xl ">1000+</span>
-            <span className="text-4xl font-bold">Collection</span>
+            <span className="lg:text-5xl md:text-3xl text-4xl ">1000+</span>
+            <span className="lg:text-4xl md:text-3xl  text-4xl font-bold">Collection</span>
           </div>
           <div className="flex flex-col">
-            <span className="text-5xl ">200+</span>
-            <span className="text-4xl font-bold">Event</span>
+            <span className="lg:text-5xl md:text-3xl  text-4xl ">200+</span>
+            <span className="lg:text-4xl md:text-3xl  text-4xl font-bold">Event</span>
           </div>
         </div>
       </div>
-      <div className="h-screen flex-auto flex flex-col justify-between">
+      <div className="h-full flex-auto flex flex-col justify-between md:px-0 px-5  gap-16">
         <div className="w-full h-[35%] bg-[#d5f178] rounded-3xl px-10 py-5">
           <span className="text-2xl h-10 flex justify-between items-center">
             <i class="ri-bar-chart-line"></i> <Avatars />
