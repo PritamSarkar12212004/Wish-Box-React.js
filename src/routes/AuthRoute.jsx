b58d7edb8605df1b45bd5b0964fa9fa4f557a325
pritam sarkar <pritam7796419792@gmail.com>
2024-09-17 20:48:56 +0530

    all ok

--- src/routes/AuthRoute.jsx
+++ src/routes/AuthRoute.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Login from '../components/auth/login/Login'
-import Register from '../components/auth/register/Register'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../components/auth/login/Login";
+import Register from "../components/auth/register/Register";
 function AuthRoute() {
   return (
-      <Routes>
-          <Route path='/auth/login' element={<Login/>}/>
-          <Route path='/auth/register' element={<Register/>}/>
+    <Routes>
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/register" element={<Register />} />
     </Routes>
-  )
+  );
 }
 
-export default AuthRoute
+export default AuthRoute;
