4887e6a6ebd96fe951ef8aa25df6761874ab39cb
pritam sarkar <pritam7796419792@gmail.com>
2024-09-23 20:17:07 +0530

    wdw

--- src/utils/axios/AxiosConfig.js
+++ src/utils/axios/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://wish-box-react-js.vercel.app",
 });
 
 export default axiosInstance;
